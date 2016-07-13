4c129c71d02eadf09092e9946ce697935e8f5020
~Haynesss~ <zerossb@gmail.com>
2016-07-13 08:57:20 -0300

    Update controllers.py

--- zeusproject/templates/app/admin/controllers.py
+++ zeusproject/templates/app/admin/controllers.py
@@ -13,17 +13,16 @@ from flask.ext.security import roles_required
 
 blueprint = Blueprint('admin', __name__, url_prefix='/admin')
 
-
-@roles_required('admin')
 @blueprint.route('/users')
+@roles_required('admin')
 def get_users():
     """return users in admin."""
     current_app.logger.debug(u'Get all users in admin.')
     return render_template('admin/users/list.html')
 
-@roles_required('admin')
 @blueprint.route('/')
+@roles_required('admin')
 def admin_page():
-    """return users in admin."""
+    """return admin page."""
     current_app.logger.debug(u'Get all users in admin.')
     return render_template('admin/index.html')
